542332c18069f6d42e116af5224bb7f576acc05d
Matthias Seghers <matthias.seghers1@student.arteveldehs.be>
2016-11-07 10:45:21 +0100

    [TASK] Add PHPUnit tests.

--- symfony/src/AppBundle/Tests/Controller/OfficeControllerTest.php
+++ symfony/src/AppBundle/Tests/Controller/OfficeControllerTest.php
@@ -6,11 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OfficeControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testOfficesSearch()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/offices');
+        $client->request('GET', '/api/v1/offices?location=Gent&range=10&weekend=N&support=N');
+        $response = $client->getResponse();
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertArrayHasKey('results', $data);
     }
 
 }
